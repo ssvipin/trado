c3faa9ce8f178f75a0383a27c0c7bcf440ddf2fc
vipin kumar <kumarvipin24031999@gmail.com>
2026-04-10 11:05:12 +0530

    tailwind css class issues

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -32,7 +32,7 @@ const UserDropdown: React.FC = () => {
                 <div className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
                     <Avatar className='h-8 w-8'>
                         <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback className='bg-yellow-500 text-yellow-900 text-small font-bold'>{user.name[0]}</AvatarFallback>
+                        <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>{user.name[0]}</AvatarFallback>
                     </Avatar>
                     <div className="hidden md:flex flex-col items-start">
                         <span className="text-base font-medium text-gray-400">
@@ -44,10 +44,10 @@ const UserDropdown: React.FC = () => {
             <DropdownMenuContent className='text-gray-400 w-auto'>
                 <DropdownMenuGroup>
                     <DropdownMenuLabel>
-                        <div className="flex relative item-center gap-3 py-2">
+                        <div className="flex relative items-center gap-3 py-2">
                             <Avatar className='h-10 w-10'>
                                 <AvatarImage src="https://github.com/shadcn.png" />
-                                <AvatarFallback className='bg-yellow-500 text-yellow-900 text-small font-bold'>{user.name[0]}</AvatarFallback>
+                                <AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>{user.name[0]}</AvatarFallback>
                             </Avatar>
                             <div className="flex flex-col">
                                 <span className="text-base font-medium text-gray-400">
@@ -60,7 +60,7 @@ const UserDropdown: React.FC = () => {
                         </div>
                     </DropdownMenuLabel>
                     <DropdownMenuSeparator className="bg-gray-600" />
-                    <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+                    <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-base font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
                         <LogOut className="h-4 w-4 mr-2 hidden sm:block"/>
                         Logout
                     </DropdownMenuItem>

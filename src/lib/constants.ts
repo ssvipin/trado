d35bbe83ec0646811278a74166f063f5ee4bb955
vipin kumar <kumarvipin24031999@gmail.com>
2026-04-10 10:54:16 +0530

    implement header

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+type navItems = {
+    href: string;
+    title: string;
+}
+export const NAV_ITEMS: navItems[] = [
+    {href:"/", title: 'Dashboard'},
+    {href:"/search", title: 'Search'},
+    {href:"/watchlist", title: 'Watchlist'}
+]
